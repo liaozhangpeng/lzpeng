9ea48e8acb3329169a6d7ae53c02cf6b408b48d9
liaozhangpeng <liaozhangpeng@gmail.com>
2018-10-10 18:02:46 +0800

    app skeleton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <header>
+          <nav>
+            <div>lzpeng</div>
+          </nav>
+        </header>
       </div>
     );
   }
